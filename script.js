f6cd280a09b4374f1f0f071ba27ccd7bdb218626
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-02-08 14:23:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	if (num === 0) {
+		return 0;
+	}
+	if (num === 1) {
+		return 1;
+	}
+	
+    let x = fibonacci(num) + fibonacci(num -1);	
+	return x;
 }
 
 module.exports = fibonacci;
